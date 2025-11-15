5ef5747ed05edde2ff3399286e7be8d91cee617f
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-15 01:51:22 +0100

    format

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,21 +1,41 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        notifier = { enabled = true },
-
-        picker = {
-            sources = {
-                explorer = {
-                    hidden = true,
-                    ignored = true,
-                    exclude = { ".DS_Store", "__pycache__", ".git", ".mypy_cache" },
-                }
-            }
-        }
-    },
-    keys = {
-        { "<leader>e", function() Snacks.explorer() end,              desc = "File Explorer" },
-        { "<leader>g", function() Snacks.lazygit() end,               desc = "LazyGit" },
-        { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notifications" },
-    }
+	"folke/snacks.nvim",
+	opts = {
+		notifier = { enabled = true },
+		image = { enabled = true },
+		scroll = { enabled = true },
+		indent = { enabled = true },
+		picker = {
+			sources = {
+				explorer = {
+					hidden = true,
+					ignored = true,
+					exclude = { ".DS_Store", "__pycache__", ".git", ".mypy_cache" },
+				},
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File Explorer",
+		},
+		{
+			"<leader>g",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "LazyGit",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notifications",
+		},
+	},
 }
