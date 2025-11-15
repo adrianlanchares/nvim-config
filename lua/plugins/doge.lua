4c855d880bca3dffbdefbad40567838be5bd63ba
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-15 10:48:14 +0100

    initial commit

---
+++ lua/plugins/doge.lua
@@ -0,0 +1,21 @@
+return {
+	"kkoomen/vim-doge",
+
+	config = function()
+		vim.g.doge_python_settings = {
+			single_quotes = 0,
+			omit_redundant_param_types = 1,
+		}
+
+		vim.g.doge_doc_standard_python = "google"
+
+		vim.g.doge_enable_mappings = 1
+	end,
+	keys = {
+		{
+			"<leader>d",
+			"<cmd>DogeGenerate<CR>",
+			desc = "Generate Python docstring",
+		},
+	},
+}
