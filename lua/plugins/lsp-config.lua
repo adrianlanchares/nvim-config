81d9fe46957ccf2a0a4f501147e1c6159b6f0de6
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-15 01:12:02 +0100

    fix encoding

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,7 +2,7 @@ return {
     {
         "mason-org/mason.nvim",
 
-        config  = function()
+        config = function()
             require("mason").setup()
         end
     },
@@ -22,9 +22,24 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.enable("lua_ls")
-            vim.lsp.enable("pyright")
-            vim.lsp.enable("ruff")
+            vim.lsp.enable("lua_ls", "pyright", "ruff")
+            vim.lsp.config("pyright",
+                { offset_encoding = "utf-16", settings = { python = { analysis = { typeChekcingMode = "off" } } } })
+
+            vim.diagnostic.config({ virtual_text = true })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover item" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition " })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+            -- Format on save if the LSP client supports it
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                callback = function(event)
+                    vim.lsp.buf.format({
+                        bufnr = event.buf,
+                        async = false,
+                    })
+                end,
+            })
         end
     }
 }
