a0d2e37ded0dc9d723edc3382e16bd28720e75de
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-15 13:40:02 +0100

    add uniform encodig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,12 +24,13 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.enable("lua_ls", "pyright", "ruff")
-			vim.lsp.config("lua_ls", { capabilities = capabilities })
+			vim.lsp.config("lua_ls", { capabilities = capabilities, offset_encoding = "utf-16" })
 			vim.lsp.config("pyright", {
 				capabilities = capabilities,
 				offset_encoding = "utf-16",
 				settings = { python = { analysis = { typeChekcingMode = "off" } } },
 			})
+			vim.lsp.config("ruff", { capabilities = capabilities, offset_encoding = "utf-16" })
 
 			vim.diagnostic.config({ virtual_text = true })
 
